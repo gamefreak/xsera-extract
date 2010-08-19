9b3ca811c28048f4a5244ae2923a369ff3b72ed3
Scott McClaugherty <gamefreak4321@gmail.com>
2010-08-19 14:04:49 -0400

    Modified extract.py to accept an argument for how many objects to print.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- extract.py
+++ extract.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+from sys import	argv
+
 import bsob
 import obac
 import race
@@ -13,6 +15,9 @@ import format
 #file = open("./data/500.snbf", "rb")
 #file = open("./data/500.snro", "rb")
 file = open("./data/500.obac", "rb")
-
-print "ret =",
-format.object(obac.parse(file,1))
+if len(argv) > 1:
+	count = int(argv[1])
+else:
+	count = 1
+for id in range(0, count):
+	format.object(obac.parse(file,1))
