76854a1baeede7010d0a84b0866e99bec24cb78b
Scott McClaugherty <gamefreak4321@gmail.com>
2011-02-04 20:00:53 -0500

    Fixed the screwy quote character, and the offset snit names.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- strings.py
+++ strings.py
@@ -11,7 +11,9 @@ def readStrings(fromFile):
 	for i in range(0, count-1):
 		len = unpack("B", fromFile.read(1))[0]
 		s = unpack(str(len) + "s", fromFile.read(len))[0]
-		strings[i] = s.replace("\\", "\\\\").replace("\"", "\\\"").replace("\r", "\\r")
+		s = s.replace("\\", "\\\\").replace("\"", "\\\"").replace("\r", "\\r")
+		s = s.replace("\xD5", "'")
+		strings[i] = s
 	return strings
 
 def get(file, id = None):
